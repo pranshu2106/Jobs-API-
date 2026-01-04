bbbaf0e50c8a00cb0882f97d0bcbcf90280e6879
pranshu2106 <pranshu2106@gmail.com>
2026-01-04 15:54:12 +0530

    Added root route with welcome message and docs link

--- app.js
+++ app.js
@@ -33,6 +33,9 @@ app.use(helmet()) ;
 // extra packages
 
 // routes
+app.get('/', (req, res) => {
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+});
 app.use('/api/v1/auth',authRouter) ;
 app.use('/api/v1/jobs',authMiddleware,jobsRouter) ; 
 
