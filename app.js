cd95d8fa0fe39b4e34d390482c7c28220e2ed97d
pranshu2106 <pranshu2106@gmail.com>
2026-01-05 21:31:21 +0530

    Added Swagger UI setup and docs file

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 require('dotenv').config();
 require('express-async-errors');
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
 const express = require('express');
 const app = express();
 
@@ -36,6 +39,8 @@ app.use(helmet()) ;
 app.get('/', (req, res) => {
   res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
 });
+app.use('/api-docs',swaggerUI.serve,swaggerUI.setup(swaggerDocument)) ; 
+
 app.use('/api/v1/auth',authRouter) ;
 app.use('/api/v1/jobs',authMiddleware,jobsRouter) ; 
 
