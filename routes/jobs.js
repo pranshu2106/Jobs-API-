534fbf34c31b65a1eeae9001c0456529a1158e78
pranshu2106 <pranshu2106@gmail.com>
2026-01-07 22:45:40 +0530

    Empty repo

--- routes/jobs.js
+++
@@ -1,8 +0,0 @@
-const express = require('express') ;
-const router = express.Router() ;
-const {getAllJobs,getSingleJob,createJob,updateJob,deleteJob} = require('../controllers/jobs') ;
-
-router.route('/').get(getAllJobs).post(createJob) ;
-router.route('/:id').get(getSingleJob).patch(updateJob).delete(deleteJob) ;
-
-module.exports = router ; 
